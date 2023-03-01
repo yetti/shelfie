70e480af04b5e8107d75f1f72c47ac464fa08529
Yetrina Battad <hello@yetti.io>
2023-03-01 16:30:24 +1100

    feat: adds static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "pages/home"
+
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unprocessable"
   get "/500", to: "errors#internal_server"
 
-  root "rails/welcome#index"
+  root "pages#home"
 end
