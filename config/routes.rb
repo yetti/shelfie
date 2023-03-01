188f7fc5c48220832f834a3cc8a8bbabf44dd051
Yetrina Battad <hello@yetti.io>
2023-03-01 13:40:13 +1100

    style: install Bootstrap and Bootstrap Icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unprocessable"
   get "/500", to: "errors#internal_server"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "rails/welcome#index"
 end
